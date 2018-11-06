8b213b0bfc5fdf61040ea7c9a67f4f737539dcbc
root <root@pihole.lxd>
2018-11-06 16:45:06 +0000

    Work in progress

---
+++ redo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export snapname=pihole  
+#export SNAPCRAFT_BUILD_ENVIRONMENT=lxd
+#export SNAPCRAFT_BUILD_ENVIRONMENT=multipass
+#export SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=24G
+#export SNAPCRAFT_BUILD_ENVIRONMENT_CPU=4
+sudo snap remove $snapname
+snapcraft clean
+if [ $? -eq 0 ];
+then
+  snapcraft | tee -a buildlog.txt
+  if [ $? -eq 0 ];
+  then
+    snap install $snapname*.snap --dangerous --devmode
+    if [ $? -eq 0 ];
+    then
+      echo "Built and installed"
+    else
+      echo "Install failed"
+    fi
+  else
+    echo "Snapcraft failed"
+  fi
+else
+  echo "Clean failed"
+fi
